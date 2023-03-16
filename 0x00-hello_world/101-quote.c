ca9961e3f8466460db3694e6bba9dfafdc59f77d
drissraki <drissrk6@gmail.com>
2023-03-16 15:13:06 -0700

    7 et 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - A c program that prints a line to the standart error
- * Return 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
-}
+ char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+ write(2, str1, 59);
+ return (1);
+}}
