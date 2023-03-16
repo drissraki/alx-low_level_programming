08d9e39c15fe29060b6f0bca5e3852704e387122
drissraki <drissrk6@gmail.com>
2023-03-16 15:05:43 -0700

    7 et 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unsistd.h>
+
+/**
+ * main - A c program that prints a line to the standart error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
